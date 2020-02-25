4a086c84e1620b2cb0449df445620ce344be5e7c
emmarito <emmagasaro@gmail.com>
2020-02-25 17:41:38 +0200

    Adding bootstrap

--- api/update_user.php
+++ api/update_user.php
@@ -29,4 +29,105 @@ $db = $database->getConnection();
 // instantiate user object
 $user = new User($db);
  
-// retrieve given jwt here
+// update_user.php
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+ 
+// get jwt
+$jwt=isset($data->jwt) ? $data->jwt : "";
+ 
+// update_user.php
+
+// if jwt is not empty
+if($jwt){
+ 
+    // if decode succeed, show user details
+    try {
+ 
+        // decode jwt
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+ 
+        // update_user.php
+
+        // set user property values
+$user->firstname = $data->firstname;
+$user->lastname = $data->lastname;
+$user->email = $data->email;
+$user->password = $data->password;
+$user->id = $decoded->data->id;
+ 
+// update_user.php
+
+// update the user record
+if($user->update()){
+    // update_user.php
+
+// we need to re-generate jwt because user details might be different
+$token = array(
+   "iss" => $iss,
+   "aud" => $aud,
+   "iat" => $iat,
+   "nbf" => $nbf,
+   "data" => array(
+       "id" => $user->id,
+       "firstname" => $user->firstname,
+       "lastname" => $user->lastname,
+       "email" => $user->email
+   )
+);
+$jwt = JWT::encode($token, $key);
+ 
+// set response code
+http_response_code(200);
+ 
+// response in json format
+echo json_encode(
+        array(
+            "message" => "User was updated.",
+            "jwt" => $jwt
+        )
+    );
+
+}
+ 
+// message if unable to update user
+else{
+    // set response code
+    http_response_code(401);
+ 
+    // show error message
+    echo json_encode(array("message" => "Unable to update user."));
+}
+    }
+ 
+    // update_user.php
+
+    // if decode fails, it means jwt is invalid
+catch (Exception $e){
+ 
+    // set response code
+    http_response_code(401);
+ 
+    // show error message
+    echo json_encode(array(
+        "message" => "Access denied.",
+        "error" => $e->getMessage()
+    ));
+}
+
+}
+ 
+// update_user.php
+
+// show error message if jwt is empty
+else{
+ 
+    // set response code
+    http_response_code(401);
+ 
+    // tell the user access denied
+    echo json_encode(array("message" => "Access denied."));
+}
+?>
+
